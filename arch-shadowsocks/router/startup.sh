713db3c841e5df4b668366039a7a7e9640160838
apporc <appleorchard2000@gmail.com>
2015-09-07 12:12:39 +0800

    process not daemonized
    Signed-off-by: apporc <appleorchard2000@gmail.com>

--- arch-shadowsocks/router/startup.sh
+++ arch-shadowsocks/router/startup.sh
@@ -1,3 +1,3 @@
 /chnroute_file/create_rules /chnroute_file/chnroute.txt
-nohup /usr/bin/ss-redir -c /etc/shadowsocks/shadowsocks-redir.json -u &
 nohup /usr/bin/ss-local -c /etc/shadowsocks/shadowsocks.json &
+/usr/bin/ss-redir -c /etc/shadowsocks/shadowsocks-redir.json -u
