24f743014fdc6513a2ca220fc9beb3ef67c7223a
apporc <appleorchard2000@gmail.com>
2015-09-07 12:32:51 +0800

    use config files volume
    Signed-off-by: apporc <appleorchard2000@gmail.com>

--- arch-shadowsocks/router/startup.sh
+++ arch-shadowsocks/router/startup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 /chnroute_file/create_rules /chnroute_file/chnroute.txt
-nohup /usr/bin/ss-local -c /etc/shadowsocks/shadowsocks.json &
+/usr/bin/ss-local -c /etc/shadowsocks/shadowsocks.json &
 /usr/bin/ss-redir -c /etc/shadowsocks/shadowsocks-redir.json -u
